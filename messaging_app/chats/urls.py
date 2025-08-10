b9c3723abce6ac381470b359a74185556b3b0998
blip <ryanniiakweibrown1@gmail.com>
2025-08-10 23:36:49 +0000

    feat: switch to NestedDefaultRouter for improved URL routing in chats

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework_nested.routers import NestedDefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
-router = routers.DefaultRouter()
+
+router = NestedDefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
