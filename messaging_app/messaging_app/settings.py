191852f8e415c05c5eb22fd72019f419702180de
blip <ryanniiakweibrown1@gmail.com>
2025-08-10 22:48:47 +0000

    feat: initialize messaging_app with ASGI, WSGI, and settings configurations

---
+++ messaging_app/messaging_app/settings.py
@@ -0,0 +1,18 @@
+# ...existing code...
+STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+}
