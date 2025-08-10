c3f3a1d2f6b4fe0eeadddcfe57fa2f7e599e555b
blip <ryanniiakweibrown1@gmail.com>
2025-08-10 23:25:24 +0000

    feat: update URL routing to include path for conversations and messages

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,4 +1,5 @@
 
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
@@ -6,4 +7,6 @@ router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+]
