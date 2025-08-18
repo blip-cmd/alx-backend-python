2d6a945b470f729227dbd3fa9e3e3432b1b47037
blip <ryanniiakweibrown1@gmail.com>
2025-08-18 16:22:59 +0000

    fix: add missing newline at end of file in urls.py and create postman-collections.json

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('api/', include('chats.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api-auth/', include('rest_framework.urls')),
 ]
