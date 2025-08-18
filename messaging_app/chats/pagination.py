834ada8a7a3d2739ede528f71bbba68c69e02459
blip <ryanniiakweibrown1@gmail.com>
2025-08-18 14:22:23 +0000

    fix: revert pagination class to use default PageNumberPagination

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -4,3 +4,7 @@ class MessagePagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return super().get_paginated_response(data)
+        # To access total count: self.page.paginator.count
